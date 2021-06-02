e5614fa3624c70a46b508f14f9b0cd863e19c23a
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-06-02 13:39:23 +0200

    setup graphql

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,32 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client.query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          rate,
+          name
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
