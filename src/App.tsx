7d94a1daa69fc4fe80f33a80ee6acd35d5fcf1d6
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-06-02 14:16:33 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { ExchangeRates } from './index';
 
 function App() {
   return (
     <div className="App">
       Hello world 
+      <ExchangeRates />
     </div>
   );
 }
